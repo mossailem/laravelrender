f6b1e5d874a23b7e7fbecc25c03032b740ff0a4b
Asrock A520m/ac Windows <muslimaswaja@yahoo.com>
2024-08-21 11:44:19 +0700

    Add dockerfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
-use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
